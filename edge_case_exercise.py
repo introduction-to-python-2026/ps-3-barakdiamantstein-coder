38901bb3e50060dde03351289a34cf009abf17c6
barakdiamantstein-coder <barakdiamantstein@gmail.com>
2025-11-11 16:41:13 +0200

    Update edge_case_exercise.py

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -1,16 +1,16 @@
 def move(my_list, direction):
-  index_of_one = my_list.index(1)
+    index_of_one = my_list.index(1)
+    new_list = my_list.copy()  # make a copy
 
-  if index_of_one == 0 and direction == 'left':
-      return(my_list)
-  elif index_of_one == len(my_list) - 1 and direction == 'right':
-      return(my_list)
-  elif direction == 'right':
-        my_list[index_of_one] = 0
-        my_list[index_of_one + 1] = 1
+    if index_of_one == 0 and direction == 'left':
+        return my_list
+    elif index_of_one == len(my_list) - 1 and direction == 'right':
+        return my_list
+    elif direction == 'right':
+        new_list[index_of_one] = 0
+        new_list[index_of_one + 1] = 1
+    elif direction == 'left':
+        new_list[index_of_one] = 0
+        new_list[index_of_one - 1] = 1
 
-  elif direction == 'left':
-        my_list[index_of_one] = 0
-        my_list[index_of_one - 1] = 1
-
-  return my_list
+    return new_list
